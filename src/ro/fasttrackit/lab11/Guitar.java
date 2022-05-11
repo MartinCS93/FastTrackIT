fca133514705a35f12c0236e68f8b42925df8941
MartinCS93 <martin.cristian93@gmai.com>
2022-05-11 19:13:22 +0300

    Initial commit

--- src/ro/fasttrackit/lab11/Guitar.java
+++ src/ro/fasttrackit/lab11/Guitar.java
@@ -1,4 +1,25 @@
 package ro.fasttrackit.lab11;
 
 public class Guitar {
+    String type; //electric or acoustic
+    double size;
+    String material;
+
+    public Guitar(String type, double size, String material) {
+        this.type = type;
+        this.size = size;
+        this.material = material;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
 }
